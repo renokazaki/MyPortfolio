14630290543234dbb23eeb9d5cdd2d4ab1e9392e
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-11-01 23:33:26 +0900

    info position fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex item-center justify-center">
+      <div className="absolute top-20 left-0 right-0 z-10 flex item-center justify-center">
         {currentStage && <Info currentStage={currentStage} />}
       </div>
       <Canvas
